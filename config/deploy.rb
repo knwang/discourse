aa6cd9b56b4baebc99be76d38b2065489230da3e
Kevin Wang <knwang@gmail.com>
2013-04-24 01:29:19 -0400

    Use services

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,10 +55,10 @@ end
 
 desc 'Starts the application'
 task :start => :environment do
-  queue "cd #{app_path}; bundle exec foreman start"
+  queue "cd #{app_path}; sudo discourse start"
 end
 
 desc 'Stops the application'
 task :stop => :environment do
-  queue "cd #{app_path}; bundle exec foreman stop"
+  queue "cd #{app_path}; sudo discourse stop"
 end
